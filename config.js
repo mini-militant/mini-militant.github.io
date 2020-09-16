d7fea1a590c01b5940613edfcce6e66b5819948a
Shailesh Kumar Singh <singhshailesh2295@gmail.com>
2020-09-16 11:04:08 +0530

    first commit

---
+++ config.js
@@ -0,0 +1,39 @@
+module.exports = {
+  siteTitle: 'Gatsby Starter Resume', // <title>
+  manifestName: 'Resume',
+  manifestShortName: 'Landing', // max 12 characters
+  manifestStartUrl: '/',
+  manifestBackgroundColor: '#663399',
+  manifestThemeColor: '#663399',
+  manifestDisplay: 'standalone',
+  manifestIcon: 'src/assets/img/website-icon.png',
+  pathPrefix: `/gatsby-starter-resume/`, // This path is subpath of your hosting https://domain/portfolio
+  firstName: 'Shailesh ',
+  lastName: 'Kumar Singh',
+  // social
+  socialLinks: [
+    {
+      icon: 'fa-github',
+      name: 'Github',
+      url: 'https://github.com/mini-militant',
+    },
+    {
+      icon: 'fa-linkedin-in',
+      name: 'Linkedin',
+      url: 'https://www.linkedin.com/in/shailesh-singh-128429144/',
+    },
+    {
+      icon: 'fa-twitter',
+      name: 'Twitter',
+      url: 'https://twitter.com/shailes2204',
+    },
+    {
+      icon: 'fa-facebook-f',
+      name: 'Facebook',
+      url: 'https://www.facebook.com/Shaileshcool',
+    },
+  ],
+  email: 'singhshailesh2295@gmail.com',
+  phone: '9482233818',
+  address: 'HSR Layout,Bangalore',
+};
---
+++ src/pages/index.js
@@ -0,0 +1,230 @@
+import React from 'react';
+
+import Layout from '../components/Layout';
+
+// import { Link } from 'gatsby';
+import Sidebar from '../components/Sidebar';
+import config from '../../config';
+const IndexPage = () => (
+  <Layout>
+    <Sidebar />
+    <div className="container-fluid p-0">
+      <section
+        className="resume-section p-3 p-lg-5 d-flex align-items-center"
+        id="about"
+      >
+        <div className="w-100">
+          <h1 className="mb-0">
+            {config.firstName}
+            <span className="text-primary">{config.lastName}</span>
+          </h1>
+          <div className="subheading mb-5">
+            {config.address} · {config.phone} ·
+            <a href={`mailto:${config.email}`}>{config.email}</a>
+          </div>
+          <p className="lead mb-5">
+            Extremely motivated to constantly develop my skills and grow
+            professionally. I am confident in my ability to come up with
+            interesting ideas.
+          </p>
+          <div className="social-icons">
+            {config.socialLinks.map(social => {
+              const { icon, url } = social;
+              return (
+                <a key={url} href={url}>
+                  <i className={`fab ${icon}`}></i>
+                </a>
+              );
+            })}
+          </div>
+        </div>
+      </section>
+
+      <hr className="m-0" />
+
+      <section
+        className="resume-section p-3 p-lg-5 d-flex justify-content-center"
+        id="experience"
+      >
+        <div className="w-100">
+          <h2 className="mb-5">Experience</h2>
+
+          <div className="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+            <div className="resume-content">
+              <h3 className="mb-0">React Web Developer</h3>
+              <div className="subheading mb-3">Suventures</div>
+              <p>
+                Worked as React Front end developer and the main responsibilities include
+                building creative dashboards, state management through redux.
+              </p>
+            </div>
+            <div className="resume-date text-md-right">
+              <span className="text-primary">June 2020 - June 2020</span>
+            </div>
+          </div>
+
+          <div className="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+            <div className="resume-content">
+              <h3 className="mb-0">Associate Software Engineer</h3>
+              <div className="subheading mb-3">Accenture</div>
+              <p>
+                Worked as a Pega Support.
+              </p>
+            </div>
+            <div className="resume-date text-md-right">
+              <span className="text-primary">May 2018 - May 2020</span>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <hr className="m-0" />
+
+      <section
+        className="resume-section p-3 p-lg-5 d-flex align-items-center"
+        id="education"
+      >
+        <div className="w-100">
+          <h2 className="mb-5">Education</h2>
+
+          <div className="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+            <div className="resume-content">
+              <h3 className="mb-0">NMAM Instiute of Technology</h3>
+              <div className="subheading mb-3">Bachelore of Engineering</div>
+              <div>Computer Science </div>
+              <p>GPA: 8.22</p>
+            </div>
+            <div className="resume-date text-md-right">
+              <span className="text-primary">August 2014 - May 2018</span>
+            </div>
+          </div>
+
+          <div className="resume-item d-flex flex-column flex-md-row justify-content-between">
+            <div className="resume-content">
+              <h3 className="mb-0">DAV Public School,BSEB</h3>
+              <div className="subheading mb-3">12th</div>
+              <p>Percentage: 80.05%</p>
+            </div>
+            <div className="resume-date text-md-right">
+              <span className="text-primary">July 2011 - Apr 2013</span>
+            </div>
+          </div>
+
+          <div className="resume-item d-flex flex-column flex-md-row justify-content-between">
+            <div className="resume-content">
+              <h3 className="mb-0">St. Pauls School,Sasaram</h3>
+              <div className="subheading mb-3">10th</div>
+              <p>CGPA: 10.0 </p>
+            </div>
+            <div className="resume-date text-md-right">
+              <span className="text-primary">Apr 2010 - Apr 2011</span>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <hr className="m-0" />
+
+      <section
+        className="resume-section p-3 p-lg-5 d-flex align-items-center"
+        id="skills"
+      >
+        <div className="w-100">
+          <h2 className="mb-5">Skills</h2>
+
+          <div className="subheading mb-3">
+            Programming Languages &amp; Tools
+          </div>
+          <ul className="list-inline dev-icons">
+            <li className="list-inline-item">
+              <i className="fab fa-html5"></i>
+            </li>
+            <li className="list-inline-item">
+              <i className="fab fa-css3-alt"></i>
+            </li>
+            <li className="list-inline-item">
+              <i className="fab fa-js-square"></i>
+            </li>
+            <li className="list-inline-item">
+              <i className="fab fa-react"></i>
+            </li>
+            <li className="list-inline-item">
+              <i className="fab fa-node-js"></i>
+            </li>
+            <li className="list-inline-item">
+              <i className="fab fa-sass"></i>
+            </li>
+            <li className="list-inline-item">
+              <i className="fab fa-npm"></i>
+            </li>
+          </ul>
+
+          <div className="subheading mb-3">Workflow</div>
+          <ul className="fa-ul mb-0">
+            <li>
+              <i className="fa-li fa fa-check"></i>
+              Mobile-First, Responsive Design
+            </li>
+            <li>
+              <i className="fa-li fa fa-check"></i>
+              Cross Browser Testing &amp; Debugging
+            </li>
+            <li>
+              <i className="fa-li fa fa-check"></i>
+              Agile Development &amp; Scrum
+            </li>
+          </ul>
+        </div>
+      </section>
+
+      <hr className="m-0" />
+
+      <section
+        className="resume-section p-3 p-lg-5 d-flex align-items-center"
+        id="interests"
+      >
+        <div className="w-100">
+          <h2 className="mb-5">Interests</h2>
+          <p>
+            Apart from being a web developer, I enjoy most of my time being
+            outdoors. I like trekking, playing cricket, travelling and finding
+            nearby adventure spots.
+          </p>
+          <p className="mb-0">
+            When forced indoors, I follow a number of sci-fi and fantasy genre
+            movies and television shows, I am an aspiring chef, and I spend a
+            large amount of my free time exploring the latest technology
+            advancements in the front-end web development world.
+          </p>
+        </div>
+      </section>
+
+      <hr className="m-0" />
+
+      <section
+        className="resume-section p-3 p-lg-5 d-flex align-items-center"
+        id="awards"
+      >
+        <div className="w-100">
+          <h2 className="mb-5">Awards &amp; Certifications</h2>
+          <ul className="fa-ul mb-0">
+            <li>
+              <i className="fa-li fa fa-trophy text-warning"></i>
+              Web Developer BootCamp - Udemy
+            </li>
+            <li>
+              <i className="fa-li fa fa-trophy text-warning"></i>
+              React Development from Scratch - Scrimba, freecodecamp.org
+            </li>
+            <li>
+              <i className="fa-li fa fa-trophy text-warning"></i>
+              St. Pauls School - Vice Captain for Emerald House 2009
+            </li>
+          </ul>
+        </div>
+      </section>
+    </div>
+  </Layout>
+);
+
+export default IndexPage;
